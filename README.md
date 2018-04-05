d2763fe64c8ed280a6bc56154a4e060901fe62c6
Sharma, Abhinav <abhsharm@visa.com>
2018-04-04 23:18:33 -0500

    demo comment classifier

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# digits-ann
-# The purpose of this project is to demostrate the power of artificial neural network in the field of image recognition. I'm using convolutional neural network model. Training sets are obtained from MNIST dataset.
+# toxic comment classifier
--- app.py
+++ app.py
@@ -20,49 +20,26 @@ graph = None
 def load_model():
     global model
     global graph
-    model = keras.models.load_model("mnist_cnn_trained.h5")
+    model = keras.models.load_model("sample.h5")
     graph = K.get_session().graph
 
 load_model()
 
-def prepare_image(image):
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-    image_size = (28, 28)
-    image = image.resize(image_size)
-    image = img_to_array(image)[:,:,0]
-    image /= 255
-    image = 1 - image
-    return image.reshape((1, 28, 28, 1))
-
-
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def post_comment():
     data = {"success": False}
     if request.method == 'POST':
-        if request.form.get('digit'):
+        if request.form.get('comment'):
             # read the base64 encoded string
-            image_string = request.form.get('digit')
-
-            # Remove the header
-            image_string = image_string.replace("data:image/png;base64,", "")
-
-            # Decode the string
-            image_data = base64.b64decode(image_string)
-
-            # Open the image
-            image = Image.open(io.BytesIO(image_data))
-
-            # Preprocess the image and prepare it for classification
-            image_preprocessed = prepare_image(image)
-
+            comment_string = request.form.get('comment')
             # Get the tensorflow default graph
             global graph
             with graph.as_default():
 
                 # Use the model to make a prediction
-                predicted_digit = model.predict_classes(image_preprocessed)[0]
-                data["prediction"] = str(predicted_digit)
+                print(comment_string);
+                classify_comment = model.predict_classes(comment_string)[0]
+                data["prediction"] = str(classify_comment)
 
                 # indicate that the request was a success
                 data["success"] = True
